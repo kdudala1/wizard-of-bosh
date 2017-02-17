f72a9b2d8074fbe2e40e715f76464cc5285bf4a6
Matt Rothenberg <mattrothenberg@gmail.com>
2017-02-16 21:15:31 -0500

    abstract d3 logic

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -11,14 +11,6 @@
 //= require components/visualization
 //= require components/vm-detail
 
-function getMetricFromString(str) {
-  if(str.length > 0) {
-    return parseInt(str.split('%')[0]);
-  } else {
-    return -1;
-  }
-}
-
 var boshVue = new Vue({
   el: '#boshviz',
   components: [
@@ -31,6 +23,12 @@ var boshVue = new Vue({
     detailShowing: false
   },
   methods: {
+    clear: function() {
+      var svg = d3.select("svg");
+      svg.select("g").remove();
+      this.vm = {};
+      this.detailShowing = false;
+    },
     setData: function(data) {
       this.json = data;
     },
@@ -38,6 +36,21 @@ var boshVue = new Vue({
       return true;
     },
     visualize: function() {
+      var self = this;
+      if(this.visualizationLoaded) {
+        this.clear();
+      }
+
+      try {
+        var d3Json = transformBoshJson(this.json);
+        buildD3(d3Json);
+      }
+
+      catch(err) {
+        console.log(err);
+        self.$message.error('Oops, we couldn\'t parse your JSON.');
+      }
+
       this.visualizationLoaded = true;
     },
     show: function(vm) {
@@ -65,7 +78,123 @@ var boshVue = new Vue({
 
       this.detailShowing = true;
       this.vm = detailedVm;
-
     }
   }
 });
+
+function buildD3(json) {
+  setTimeout(function() {
+    var svg = d3.select("svg"),
+      diameter = +svg.attr("width"),
+      g = svg.append("g").attr("transform", "translate(1,1)"),
+      format = d3.format(",d");
+
+    var bod = d3.select('#vis-wrap')
+      .call(d3.zoom().on('zoom', function() {
+        g.attr('transform', d3.event.transform)
+      }))
+
+    var pack = d3.pack()
+      .padding(35)
+      .size([diameter - 4, diameter - 4]);
+
+    var root = json;
+
+    root = d3.hierarchy(root)
+        .sum(function(d) { return d.size; })
+        .sort(function(a, b) { return b.value - a.value; });
+
+    var node = g.selectAll(".node")
+      .data(pack(root).descendants())
+      .enter().append("g")
+        .attr("class", function(d) { return d.children ? "node" : "leaf node"; })
+        .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
+
+    node.append("title")
+      .text(function(d) { return d.data.name + "\n" + format(d.value); });
+
+    node.append("circle")
+    .attr("r", function(d) { return (d.r); })
+    .attr("class", function(d) {
+      return d.data.status
+    });
+
+    d3.selectAll(".leaf.node circle").on("click", function (d) {
+      boshVue.show(d.data);
+      var activeClass = "active";
+      var alreadyIsActive = d3.select(this.parentNode).classed(activeClass);
+      svg.selectAll(".leaf.node")
+        .classed(activeClass, false);
+      d3.select(this.parentNode).classed(activeClass, !alreadyIsActive);
+    });
+
+    node.filter(function(d) { return !d.children; }).append("text")
+        .attr("dy", "0.3em")
+        .text(function(d) { return d.data.name.substring(0, d.r / 3); });
+  })
+}
+
+function pullProcesses(rows) {
+  var processes = [];
+
+  _.each(rows, function(row) {
+    if( row[1].length > 0 ) {
+      processes.push({
+        instance: row[0],
+        process: row[1],
+        uptime: row[14],
+        processState: row[2],
+        memory: getMetricFromString(row[20]),
+        az: row[3],
+        ips: row[4]
+      });
+    }
+  });
+
+  return processes;
+}
+
+function transformBoshJson(json) {
+  var parsedJson = JSON.parse(json);
+  var d3Json = {
+    name: 'vms',
+    children: []
+  };
+
+  _.each(parsedJson.Tables, function(table) {
+    var processes = pullProcesses(table.Rows);
+    var vms = [];
+
+    _.each(table.Rows, function(row) {
+      if(row[1].length == '') {
+        var rowProcesses = _.filter(processes, function(process) {
+          return process.instance == row[0];
+        });
+
+        vms.push({
+          name: row[0].split('/')[0],
+          status: row[2],
+          details: row,
+          processes: rowProcesses,
+          size: 200
+        });
+
+      }
+    });
+
+    d3Json.children.push({
+      name: 'depl',
+      children: vms
+    });
+  });
+
+  return d3Json;
+}
+
+function getMetricFromString(str) {
+  if(str.length > 0) {
+    return parseInt(str.split('%')[0]);
+  } else {
+    return -1;
+  }
+}
