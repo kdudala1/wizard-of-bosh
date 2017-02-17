e30c611af7965f3fd23a5f3635437844344a0abe
Matt Rothenberg <mattrothenberg@gmail.com>
2017-02-16 19:03:16 -0500

    Design tweaks, add Element UI

--- source/javascripts/components/json-textarea.js
+++ source/javascripts/components/json-textarea.js
@@ -1,3 +1,14 @@
 Vue.component('json-textarea', {
-  template: '#json-textarea'
+  template: '#json-textarea',
+  mounted: function() {
+    var editor = document.getElementById('editor');
+    var jsonEditor = CodeMirror(editor, {
+      value: '// $ bosh instances --ps --vitals --details --json',
+      lineNumbers: true,
+      matchBrackets: true,
+      autoCloseBrackets: true,
+      mode: { name: "javascript", json: true },
+      theme: 'eclipse'
+    });
+  }
 })
