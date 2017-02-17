fc6f06b1cd5424a818578027d3c8be7dc7f93f66
Matt Rothenberg <mattrothenberg@gmail.com>
2017-02-16 21:34:20 -0500

    Solve disabled button dilemma

--- source/javascripts/components/json-textarea.js
+++ source/javascripts/components/json-textarea.js
@@ -1,11 +1,17 @@
+var INITIAL_EDITOR_VALUE = '// $ bosh instances --ps --vitals --details --json';
+
 var jsonTextarea = Vue.component('json-textarea', {
   template: '#json-textarea',
   data: function() {
     return {
-      editor: ''
+      editor: '',
+      editorValue: INITIAL_EDITOR_VALUE
     }
   },
   methods: {
+    needsJson: function() {
+      return this.editorValue == INITIAL_EDITOR_VALUE || this.editorValue.length == 0;
+    },
     visualize: function() {
       boshVue.visualize();
     },
@@ -29,7 +35,7 @@ var jsonTextarea = Vue.component('json-textarea', {
     var self = this;
     var editor = document.getElementById('editor');
     var jsonEditor = CodeMirror(editor, {
-      value: '// $ bosh instances --ps --vitals --details --json',
+      value: INITIAL_EDITOR_VALUE,
       lineNumbers: true,
       matchBrackets: true,
       autoCloseBrackets: true,
@@ -41,6 +47,7 @@ var jsonTextarea = Vue.component('json-textarea', {
 
     self.editor.on('changes', function() {
       var val = jsonEditor.getValue();
+      self.editorValue= val;
       self.setParentData(val);
     });
   }
